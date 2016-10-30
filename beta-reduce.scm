6ab95fa8d61e43e2499abaa003c8f378cef23dc0
nobutaka <n.takushima@gmail.com>
2016-10-30 18:05:09 +0900

    handle quote to support improper list.

--- beta-reduce.scm
+++ beta-reduce.scm
@@ -9,41 +9,46 @@
   (lambda (x)
     (or
       (match x
-             (('quote liteal) #t)
-             ((f . args)
-              (if (primitive-operator? f) #t #f))
-             (_ #f))
+             [('quote liteal) #t]
+             [(f . args)
+              (if (primitive-operator? f) #t #f)]
+             [_ #f])
       (and (not (pair? x)) (not (null? x))))))
 
 (define (beta-reduce expr)
-  (if (not (pair? exp))
-      expr
-      (match expr
-        ((('lambda formals body) . args)
-         (if (every atomic? args)  ; symbol? or constant? other than lamnda-expr
-             (let ((body-u (subst (map cons formals args) body)))
-               (if (equal? body body-u)
-                   body
-                   (beta-reduce body-u)))
-             `((lambda ,formals ,(beta-reduce body))
-               ,@(map beta-reduce args))))
-        ((? symbol?) expr)
-        (('lambda formals body)
-         `(lambda ,formals ,(beta-reduce body)))
-        ((f . args)
-         (map beta-reduce expr))
-        (_ expr))))
+  (match expr
+    [('quote obj)
+     expr]
+    [(('lambda formals body) . args)
+     (if (every atomic? args)  ; symbol? or constant? other than lamnda-expr
+         (let ((body-u (subst (map cons formals args) body)))
+           (if (equal? body body-u)
+               body
+               (beta-reduce body-u)))
+         `((lambda ,formals ,(beta-reduce body))
+           ,@(map beta-reduce args)))]
+    [(? symbol?)
+     expr]
+    [('lambda formals body)
+     `(lambda ,formals ,(beta-reduce body))]
+    [(f . args)
+     (map beta-reduce expr)]
+    [_
+     expr]))
 
 (define (subst s expr)
   (match expr
-    ((? symbol?)
+    [('quote obj)
+     expr]
+    [(? symbol?)
      (let ((p (assq expr s)))
-       (if p (cdr p) expr)))
-    (('lambda formals body)
+       (if p (cdr p) expr))]
+    [('lambda formals body)
      (let ((s-u (filter (lambda (p) (not (memq (car p) formals))) s)))
-       `(lambda ,formals ,(subst s-u body))))
-    (('set! var e)
-     `(set! ,var ,(subst s e)))
-    ((f . args)
-     (map (lambda (e) (subst s e)) expr))
-    (_ expr)))
+       `(lambda ,formals ,(subst s-u body)))]
+    [('set! var e)
+     `(set! ,var ,(subst s e))]
+    [(f . args)
+     (map (lambda (e) (subst s e)) expr)]
+    [_
+     expr]))

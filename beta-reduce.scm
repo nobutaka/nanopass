9a8b18a4d0df840cd9a6a65cbbb8a05891a4e875
nobutaka <n.takushima@gmail.com>
2016-10-30 21:04:05 +0900

    refactor

--- beta-reduce.scm
+++ beta-reduce.scm
@@ -10,47 +10,47 @@
     (or
       (match x
              [('quote liteal) #t]
-             [(f . args)
-              (if (primitive-operator? f) #t #f)]
-             [_ #f])
+             [(rator . rands)
+              (if (primitive-operator? rator) #t #f)]
+             [else #f])
       (and (not (pair? x)) (not (null? x))))))
 
 (define beta-reduce
-  (lambda (expr)
-    (match expr
+  (lambda (exp)
+    (match exp
       [('quote obj)
-       expr]
+       exp]
       [(('lambda formals body) . args)
-       (if (every atomic? args)  ; symbol? or constant? other than lamnda-expr
-           (let ((body-u (subst (map cons formals args) body)))
+       (if (every atomic? args)  ; symbol? or constant? other than lamnda-exp
+           (let ([body-u (subst (map cons formals args) body)])
              (if (equal? body body-u)
                  body
                  (beta-reduce body-u)))
            `((lambda ,formals ,(beta-reduce body))
              ,@(map beta-reduce args)))]
       [(? symbol?)
-       expr]
+       exp]
       [('lambda formals body)
        `(lambda ,formals ,(beta-reduce body))]
-      [(f . args)
-       (map beta-reduce expr)]
-      [_
-       expr])))
+      [(rator . rands)
+       (map beta-reduce exp)]
+      [else
+       exp])))
 
 (define subst
-  (lambda (s expr)
-    (match expr
-      [('quote obj)
-       expr]
+  (lambda (s exp)
+    (match exp
       [(? symbol?)
-       (let ((p (assq expr s)))
-         (if p (cdr p) expr))]
+       (let ([p (assq exp s)])
+         (if p (cdr p) exp))]
+      [('quote obj)
+       exp]
+      [('set! v e)
+       `(set! ,v ,(subst s e))]
       [('lambda formals body)
-       (let ((s-u (filter (lambda (p) (not (memq (car p) formals))) s)))
+       (let ([s-u (filter (lambda (p) (not (memq (car p) formals))) s)])
          `(lambda ,formals ,(subst s-u body)))]
-      [('set! var e)
-       `(set! ,var ,(subst s e))]
-      [(f . args)
-       (map (lambda (e) (subst s e)) expr)]
-      [_
-       expr])))
+      [(rator . rands)
+       (map (lambda (e) (subst s e)) exp)]
+      [else
+       exp])))

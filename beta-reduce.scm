6b424bc13e382a755f20a481a230456c1354fec9
nobutaka <n.takushima@gmail.com>
2016-10-30 22:50:38 +0900

    refactor

--- beta-reduce.scm
+++ beta-reduce.scm
@@ -1,35 +1,25 @@
 (use util.match)
 
-(define primitive-operator?
-  (lambda (rator)
-    (and (symbol? rator)
-         (memq rator *prim-names*))))
-
 (define atomic?
   (lambda (x)
-    (or
-      (match x
-             [('quote liteal) #t]
-             [(rator . rands)
-              (if (primitive-operator? rator) #t #f)]
-             [else #f])
-      (and (not (pair? x)) (not (null? x))))))
+    (or (not (pair? x))
+        (eq? 'quote (car x)))))
 
 (define beta-reduce
   (lambda (exp)
     (match exp
+      [(? symbol?)
+       exp]
       [('quote obj)
        exp]
       [(('lambda formals body) . args)
-       (if (every atomic? args)  ; symbol? or constant? other than lamnda-exp
+       (if (every atomic? args)
            (let ([body-u (subst (map cons formals args) body)])
              (if (equal? body body-u)
                  body
                  (beta-reduce body-u)))
            `((lambda ,formals ,(beta-reduce body))
              ,@(map beta-reduce args)))]
-      [(? symbol?)
-       exp]
       [('lambda formals body)
        `(lambda ,formals ,(beta-reduce body))]
       [(rator . rands)

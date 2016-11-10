e30a304b6539b4503b036e4dd3ecf89a4e3e42f6
nobutaka <n.takushima@gmail.com>
2016-11-11 04:22:34 +0900

    Add fib sample

---
+++ samples/fib.scm
@@ -0,0 +1,7 @@
+(letrec ((fib (lambda (n)
+                (if (= n 0)
+                    n
+                    (if (= n 1)
+                        n
+                        (+ (fib (- n 1)) (fib (- n 2))))))))
+  (fib 35))

2bef45e09811429440c599ae88142c4d158a1b49
nobutaka <n.takushima@gmail.com>
2016-11-15 01:57:52 +0900

    Step: fifth and final attempt

--- samples/tinyrenderer.scm
+++ samples/tinyrenderer.scm
@@ -8,11 +8,31 @@
 
 (define line
   (lambda (x0 y0 x1 y1 image color)
-    (do ((t 0.0 (fl+ t 0.01)))
-        ((fl<= 1.0 t) #t)
-      (let ([x (fixnum (fl+ (fl* (flonum x0) (fl- 1.0 t)) (fl* (flonum x1) t)))]
-            [y (fixnum (fl+ (fl* (flonum y0) (fl- 1.0 t)) (fl* (flonum y1) t)))])
-        (tgaimage_set_r_g_b image x y (vector-ref color 0) (vector-ref color 1) (vector-ref color 2))))))
+    (let ([steep #f])
+      (if (fx< (fxabs (fx- x0 x1)) (fxabs (fx- y0 y1)))
+          (begin (swap! x0 y0)
+                 (swap! x1 y1)
+                 (set! steep #t))
+          #f)
+      (if (fx< x1 x0)
+          (begin (swap! x0 x1)
+                 (swap! y0 y1))
+          #f)
+      (let ([dx (fx- x1 x0)]
+            [dy (fx- y1 y0)])
+        (let ([derror2 (fx* (fxabs dy) 2)]
+              [error2 0]
+              [y y0])
+          (do ((x x0 (fx+ x 1)))
+              ((fx< x1 x) #t)
+            (if steep
+                (tgaimage_set_r_g_b image y x (vector-ref color 0) (vector-ref color 1) (vector-ref color 2))
+                (tgaimage_set_r_g_b image x y (vector-ref color 0) (vector-ref color 1) (vector-ref color 2)))
+            (set! error2 (fx+ error2 derror2))
+            (if (fx< dx error2)
+                (begin (set! y (fx+ y (if (fx< y0 y1) 1 -1)))
+                       (set! error2 (fx- error2 (fx* dx 2))))
+                #f)))))))
 
 (let ([image (tgaimage_new_w_h_bpp 100 100 3)])
   (line 13 20 80 40 image '#(255 255 255))
